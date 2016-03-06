5dd8f9bc530e4284439ac1664c567ce070ea7ce6
jleewebdev <lee45276@gmail.com>
2016-03-06 15:22:36 +0800

    Added styling

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express'),
+  path = require("path"),
+  fs = require("fs"),
+  file_path = path.resolve(path.dirname(__dirname), "data/albums.json"),
+  router = express.Router();
+
+function getAlbums() {
+  return JSON.parse(fs.readFileSync(file_path, "utf8"));
+}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    albums: getAlbums()
+  });
 });
 
 module.exports = router;
